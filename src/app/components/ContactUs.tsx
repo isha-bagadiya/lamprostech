a5d3cfd0896ac913a643eb5ffc7d3f019e32a50c
isha-bagadiya <ishabagadiya@lampos.tech>
2024-05-24 10:31:13 +0530

    footer and contactus component added

---
+++ src/app/components/ContactUs.tsx
@@ -0,0 +1,42 @@
+import styles from '../styles/Contact.module.css';
+
+export default function ContactUs() {
+  return (
+      <div className={styles.container}>
+        <div className={styles.innerMainDiv}>
+        <div className={styles.leftContent}>
+        <h1 className={styles.title}>Contact Us</h1>
+        <p className={styles.description}>
+        Drop us a line to enquire about our services for your new software launch or your latest software innovation. We'll get back to you within a business day
+        </p>
+        </div>
+
+        <div className={styles.formContainer}>
+          <form className={styles.form}>
+            <input type="text" name="name" placeholder="Your Name" className={styles.input} />
+            <input type="email" name="email" placeholder="Email Address" className={styles.input} />
+            <input type="text" name="phone" placeholder="Contact Number" className={styles.input} />
+            <p className={styles.description2}>Select your country</p>
+            <select name="country" className={styles.select}>
+              <option value="usa">U.S.A</option>
+              <option value="india">India</option>
+              <option value="europe">Europe</option>
+              <option value="uk">UK</option>
+              <option value="canada">Canada</option>
+              <option value="singapore">Singapore</option>
+              <option value="australia">Australia</option>
+            </select>
+            <p className={styles.description2}>Select your Budget</p>
+            <select name="budget" className={styles.select}>
+              <option value="1-5000">$1,000 - $5,000</option>
+              <option value="1-5000">$5,000 - $10,000</option>
+              <option value="1-5000">More than $10,000</option>
+            </select>
+            <textarea name="description" placeholder="Project Description" className={styles.textarea}></textarea>
+            <button type="submit" className={styles.button}>Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React from "react";
+import Image from 'next/image';
+import Logo from "../../../public/dark_logo.webp"
+import "../styles/footer.css"
+import { FaInstagram } from "react-icons/fa";
+import { BsBehance } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { IoIosArrowUp } from "react-icons/io";
+import ContactUs from "./ContactUs";
 
 const Footer = () => {
   return (
-    <div>
-      footer
-    </div>
-  )
-}
+    <>
+      <div className="footer-div">
+        <ContactUs />
+        <div className="footer-upper">
+        <div className="footer-left">
+          <div>
+            <Image src={Logo} alt="logo" className="footer-img"></Image>
+          </div>
+          <div className="content-left">
+          Lampros Tech is a leading Blockchain Development Company with highly skilled group of professionals. Let us simplify blockchain and all-things Web 3.0 for you!
+          </div>
+          <div className="icons">
+            <div className="icon-style"><FaInstagram className="icon"/></div>
+            <div className="icon-style"><FaLinkedin className="icon"/></div>
+            <div className="icon-style"><FaTwitter className="icon"/></div>
+            <div className="icon-style"><BsBehance className="icon"/></div>
+          </div>
+        </div>
+        <div className="footer-right">
+          <div>
+            <header className="header">Company</header>
+            <ul className="contact-list">
+              <li style={{color: '#29A19c'}}>Home</li>
+              <li>About Us</li>
+              <li>Contact Us</li>
+              <li>Careers</li>
+              <li>Blog</li>
+              <li>Case Studies</li>
+              <li>Sitemap</li>
+            </ul>
+          </div>
+          <div>
+            <header className="expertise">Expertise</header>
+            <ul className="expertise-list">
+              <li>Smart Contract Development</li>
+              <li>Defi Development</li>
+              <li>NFT Marketplace Development</li>
+              <li>NFT Consulting</li>
+              <li>Blockchain Development Consulting</li>
+              <li>dApps Development</li>
+              <li>Full Stack Development</li>
+              <li>Web Development</li>
+            </ul>
+          </div>
+        </div>
+        </div>
+        <div className="up-scroller"><div className="scroll-nested"><IoIosArrowUp /></div></div>
+        <div className="copyright">© 2024 Lampros Tech. All Rights Reserved.</div>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
